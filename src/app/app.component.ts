bf0862ca44a2dd3edced8fe334245c58d51fbebe
tcampbe <tcampb@icloud.com>
2020-01-12 19:42:30 -0600

    all instructions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 // export class AppComponent {
 //   title = 'NG421wk1day1ToDoApp';
 // }
 export class AppComponent {
   title = "Todos";
-  todoList: any [] = [];
+  todoList: any[] = [];
+  todoTitle: string;
+  ngOnInit() {
+    this.todoTitle = "";
+    this.todoList = [{ title: "Install Angular CLI", isDone: false }];
+  }
+  // add method
+  addTodo():void {
+    this.todoList.push({
+      title: this.todoTitle,
+    isDone: false
+    });
+    //reset to empty string
+    this.todoTitle = '';
+  }
+  // delete method
+  deleteTodo(todo: any) {
+    const index = this.todoList.findIndex(todoItem => todoItem === todo);
+    this.todoList.splice(index, 1);
+  }
 }
