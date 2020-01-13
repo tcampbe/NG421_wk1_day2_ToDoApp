ccef0353673609138f8668317c22140a3dfc762e
tcampbe <tcampb@icloud.com>
2020-01-12 19:04:01 -0600

    bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+// export class AppComponent {
+//   title = 'NG421wk1day1ToDoApp';
+// }
 export class AppComponent {
-  title = 'NG421wk1day1ToDoApp';
+  title = "Todos";
+  todoList: any [] = [];
 }
